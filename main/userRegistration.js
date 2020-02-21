24c40a4fc6a1eb476f9e2fb5a54ff838d34a1f7e
smitagaikawad25 <gaikawadsmita021@gmail.com>
2020-02-21 11:54:05 +0530

    Added: functionality to validate last name

--- main/userRegistration.js
+++ main/userRegistration.js
@@ -2,5 +2,10 @@ module.exports = {
     analyseName(name) {
         var pattern = /^[A-Z]{1}[a-z]{3,}/;
         return pattern.test(name);
+    },
+
+    analyseLastName(Lastname) {
+        var pattern = /^[A-Z]{1}[a-z]{3,}/;
+        return pattern.test(Lastname);
     }
 }
--- test/userRegistrationTest.js
+++ test/userRegistrationTest.js
@@ -23,3 +23,27 @@ describe('Test Case For validation of name', () =>{
         assert.isFalse(name)
     })
 })
+
+describe('Test Case For validation of last name', () =>{
+
+    it('given last name when in proper format should return true',() =>{
+        var name = userRegistration.analyseName('Shinde')
+        assert.isTrue(name)
+    })
+
+    it('given last name when not in proper format should return false',() =>{
+        var name = userRegistration.analyseName('shinde')
+        assert.isFalse(name)
+    })
+
+    it('given last name when not in proper format should return false',() =>{
+        var name = userRegistration.analyseName('Shi12')
+        assert.isFalse(name)
+    })
+
+    it('given last name start with number should return false',() =>{
+        var name = userRegistration.analyseName('12shi')
+        assert.isFalse(name)
+    })
+})
+
