07498946eca6b195048690b83f3790d2f3ea91bf
smitagaikawad25 <gaikawadsmita021@gmail.com>
2020-02-21 12:38:25 +0530

    Added: function to validate mail id

--- main/userRegistration.js
+++ main/userRegistration.js
@@ -7,5 +7,10 @@ module.exports = {
     analyseLastName(Lastname) {
         var pattern = /^[A-Z]{1}[a-z]{3,}/;
         return pattern.test(Lastname);
+    },
+
+    analyseEmailId(mailId) {
+        var pattern = /^[a-zA-Z]{1,}([.]{1})?[0-9a-zA-Z]*[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-zA-Z]{2,4}([.]{1})?([a-zA-Z]{2,3})?$/;
+        return pattern.test(mailId)
     }
 }
--- test/userRegistrationTest.js
+++ test/userRegistrationTest.js
@@ -1,49 +1,57 @@
 var assert = require('chai').assert
 var userRegistration = require('../main/userRegistration')
 
-describe('Test Case For validation of first name', () =>{
+describe('Test Case For validation of first name', () => {
 
-    it('given first name when in proper format should return true',() =>{
-        var name = userRegistration.analyseName('Smita')
+    it('given first name when in proper format should return true', () => {
+        var name = userRegistration.analyseFirstName('Smita')
         assert.isTrue(name)
     })
 
-    it('given first name when not in proper format should return false',() =>{
-        var name = userRegistration.analyseName('smita')
+    it('given first name when not in proper format should return false', () => {
+        var name = userRegistration.analyseFirstName('smita')
         assert.isFalse(name)
     })
 
-    it('given first name when not in proper format should return false',() =>{
-        var name = userRegistration.analyseName('Smi12')
+    it('given first name when not in proper format should return false', () => {
+        var name = userRegistration.analyseFirstName('Smi12')
         assert.isFalse(name)
     })
 
-    it('given first name start with number should return false',() =>{
-        var name = userRegistration.analyseName('12smi')
+    it('given first name start with number should return false', () => {
+        var name = userRegistration.analyseFirstName('12smi')
         assert.isFalse(name)
     })
 })
 
-describe('Test Case For validation of last name', () =>{
+describe('Test Case For validation of last name', () => {
 
-    it('given last name when in proper format should return true',() =>{
-        var name = userRegistration.analyseName('Shinde')
+    it('given last name when in proper format should return true', () => {
+        var name = userRegistration.analyseLastName('Shinde')
         assert.isTrue(name)
     })
 
-    it('given last name when not in proper format should return false',() =>{
-        var name = userRegistration.analyseName('shinde')
+    it('given last name when not in proper format should return false', () => {
+        var name = userRegistration.analyseLastName('shinde')
         assert.isFalse(name)
     })
 
-    it('given last name when not in proper format should return false',() =>{
-        var name = userRegistration.analyseName('Shi12')
+    it('given last name when not in proper format should return false', () => {
+        var name = userRegistration.analyseLastName('Shi12')
         assert.isFalse(name)
     })
 
-    it('given last name start with number should return false',() =>{
-        var name = userRegistration.analyseName('12shi')
+    it('given last name start with number should return false', () => {
+        var name = userRegistration.analyseLastName('12shi')
         assert.isFalse(name)
+    })
+})
+
+describe('Test Case to validate mail id', () => {
+
+    it('given mail id when in proper format should return true', () => {
+        var mailId = userRegistration.analyseEmailId('smita.shinde123@gmail.com.in')
+        assert.isTrue(mailId)
     })
 })
 
